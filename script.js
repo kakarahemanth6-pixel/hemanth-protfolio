a9bdf97a4cabaf78ce636b501dc759b329814345
kakarahemanth6-pixel <kakarahemanth6@gmail.com>
2026-09-16 13:26:55 +0530

    Remove smooth scroll functionality from script.js

--- script.js
+++ script.js
@@ -1,8 +1,1 @@
-document.querySelectorAll('a[href^="#"]').forEach(a =>  {
-  a.addEventListener('click', (e) =>   {
-    e.preventDefault();
-    document.querySelector(a.getAttribute('href')).scrollIntoView({behavior:'smooth'});
-  });
-});
 
-console.log("portfolio loaded-Hemanth.dev🔥");
