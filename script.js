7190e9a63ce9c93b07ba04bd7bf5e0b7c398e890
kakarahemanth6-pixel <kakarahemanth6@gmail.com>
2026-09-17 20:46:38 +0530

    Add orderNow function to create WhatsApp link

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
+function orderNow(item){
+  let phone = "917799246590"; 
+  let message = "Hi bro! Naku " + item + " kavali. Order cheyava? 🔥";
+  let whatsappLink = "https://wa.me/" + phone + "?text=" + encodeURIComponent(message);
+  
+  window.open(whatsappLink, "_blank");
+}
 
